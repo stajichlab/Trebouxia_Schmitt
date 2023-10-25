71dd9a2c44d0445d992e36d153746c67403565d9
Jason Stajich <jasonstajich.phd@gmail.com>
2023-10-25 14:42:41 -0700

    update methods for generating other pairwise

---
+++ pipeline/duplication/03_gbk_split_clinker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -c 2 --mem 8gb --out logs/clinker_split.log
+
+module load biopython
+
+
---
+++ pipeline/duplication/alt_hap02_vs_hap01.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -N 1 -n 1 -c 12 --mem 24G --out logs/duplication_hap02_hap01.log -p epyc
+module load fasta
+module load workspace/scratch
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+    CPU=$SLURM_CPUS_ON_NODE
+fi
+
+ANNOT=annotation
+SAMPLES=samples.csv
+OUT=duplication/search_hap02_hap01
+EVALUE=1e-15
+CDS=cds
+PEP=pep
+mkdir -p $CDS $PEP $OUT
+NAME=Trebouxia_sp._hap02_hap01
+
+if [ ! -s $OUT/$NAME.FASTA.tab.gz ]; then
+	fasta36 -T $CPU -m 8c -E $EVALUE $PEP/Trebouxia_sp._Hap2.proteins.fa $PEP/Trebouxia_sp._Hap01.proteins.fa > $OUT/$NAME.FASTA.tab
+	pigz $OUT/$NAME.FASTA.tab
+fi
